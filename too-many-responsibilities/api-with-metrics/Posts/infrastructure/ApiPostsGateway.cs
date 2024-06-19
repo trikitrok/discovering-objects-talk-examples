ce406dad8c9b30b2e3c031b41ae5d6e1e6535743
trikitrok <trikitrok@gmail.com>
2024-06-19 11:00:10 +0200

    extracting generic client

--- too-many-responsibilities/api-with-metrics/Posts/infrastructure/ApiPostsGateway.cs
+++ too-many-responsibilities/api-with-metrics/Posts/infrastructure/ApiPostsGateway.cs
@@ -1,8 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Net;
-using System.Net.Http;
-using System.Text.Json;
 using UserAccount;
 
 namespace Posts.infrastructure;
@@ -22,11 +19,12 @@ public class ApiPostsGateway : PostsGateway
         var posts = new List<Post>();
         try
         {
-            using var client = new HttpClient();
-            var response = RequestResponse(client, CreateUriFor(user));
-            var responseStream = response.Content.ReadAsStreamAsync().Result;
-            var responseData = JsonSerializer.DeserializeAsync<List<PostApiResponse>>(responseStream).Result;
+            var apiClient = new HttpGenericApiClient<PostData>();
+
+            var uri = CreateUriFor(user);
             
+            var responseData = apiClient.GetApiResponse(uri);
+
             foreach (var postResponse in responseData)
             {
                 var post = CreatePostFrom(postResponse);
@@ -40,16 +38,7 @@ public class ApiPostsGateway : PostsGateway
         return posts;
     }
 
-    private HttpResponseMessage RequestResponse(HttpClient client, string uri)
-    {
-        var response = client.GetAsync(uri).Result;
-        if (response.StatusCode != HttpStatusCode.OK) {
-            throw new APiErrorResponseException( response.StatusCode.ToString());
-        }
-        return response;
-    }
-
-    private Post CreatePostFrom(PostApiResponse response)
+    private Post CreatePostFrom(PostData response)
     {
         return new Post(new Id(response.postId), response.title, response.text, new Id(response.userId));
     }
@@ -59,7 +48,7 @@ public class ApiPostsGateway : PostsGateway
         return _apiBaseUrl + "/posts?version=" + ApiVersion + "&" + user.Id().AsText();
     }
     
-    private class PostApiResponse
+    private class PostData
     {
         public string userId { get; set; }
         public string postId { get; set; }
---
+++ too-many-responsibilities/api-with-metrics/Posts/infrastructure/HttpGenericApiClient.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text.Json;
+
+namespace Posts.infrastructure;
+
+internal interface GenericApiClient<T>
+{
+    List<T> GetApiResponse(string uri);
+}
+
+class HttpGenericApiClient<T> : GenericApiClient<T>
+{
+    public List<T> GetApiResponse(string uri)
+    {
+        using var client = new HttpClient();
+        var response1 = client.GetAsync(uri).Result;
+        if (response1.StatusCode != HttpStatusCode.OK) {
+            throw new APiErrorResponseException( response1.StatusCode.ToString());
+        }
+
+        var response = response1;
+        var responseStream = response.Content.ReadAsStreamAsync().Result;
+        return JsonSerializer.DeserializeAsync<List<T>>(responseStream).Result;
+    }
+}
