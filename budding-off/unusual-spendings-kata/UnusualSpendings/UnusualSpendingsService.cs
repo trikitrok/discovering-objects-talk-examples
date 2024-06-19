18420b4ae924328b1f6008a81046af8c653af97d
trikitrok <trikitrok@gmail.com>
2024-06-20 01:34:04 +0200

    extracting helper methods and making some proximity refactorings

--- budding-off/unusual-spendings-kata/UnusualSpendings/UnusualSpendingsService.cs
+++ budding-off/unusual-spendings-kata/UnusualSpendings/UnusualSpendingsService.cs
@@ -9,8 +9,9 @@ public class UnusualSpendingsService
     private readonly UserRepository _userRepository;
     private readonly AlertSender _alertsSender;
     private readonly UnusualSpendingsDetector _unusualSpendingsDetector;
-    
-    public UnusualSpendingsService(UnusualSpendingsDetector unusualSpendingsDetector, UserRepository userRepository, AlertSender alertsSender)
+
+    public UnusualSpendingsService(UnusualSpendingsDetector unusualSpendingsDetector, UserRepository userRepository,
+        AlertSender alertsSender)
     {
         _unusualSpendingsDetector = unusualSpendingsDetector;
         _userRepository = userRepository;
@@ -20,21 +21,50 @@ public class UnusualSpendingsService
     public void Alert(User user)
     {
         var unsusualSpendings = _unusualSpendingsDetector.Detect(user);
-        if (unsusualSpendings.Count == 0)
+        if (IsEmpty(unsusualSpendings))
         {
             return;
         }
 
+        var alert = CreateAlert(unsusualSpendings, _userRepository.GetContactData(user));
+        _alertsSender.Send(alert);
+    }
+
+    private Alert CreateAlert(List<UnsusualSpending> unsusualSpendings, UserContactData contactData)
+    {
+        var alertText = ComposeAlertText(unsusualSpendings, contactData, out var userContactData);
+        var alert = new Alert(alertText, userContactData);
+        return alert;
+    }
+
+    private string ComposeAlertText(List<UnsusualSpending> unsusualSpendings, UserContactData contactData,
+        out UserContactData userContactData)
+    {
+        var alertText = Greeting();
         var unsusualSpending = unsusualSpendings.First();
-        var userContactData = _userRepository.GetContactData(user);
+        userContactData = contactData;
         var spendingCategories = unsusualSpending.SpendingCategories();
         var spendingCategory = spendingCategories.First();
-        var alertText =
-            $"Hello card user!\n\n"+
-            "We have detected unusually high spending on your card in these categories:\n\n"+
-            $"* You spent {spendingCategory.CurrencySymbol()}"+
-            $"{spendingCategory.TotalAmountspent().ToString("f2", new CultureInfo("en-US"))} "+
-            $"on {spendingCategory.Name()}\n\nLove,\n\nThe Credit Card Company\n";
-        _alertsSender.Send(new Alert(alertText, userContactData));
+        alertText += "We have detected unusually high spending on your card in these categories:\n\n" +
+                     $"* You spent {spendingCategory.CurrencySymbol()}" +
+                     $"{spendingCategory.TotalAmountspent().ToString("f2", new CultureInfo("en-US"))} " +
+                     $"on {spendingCategory.Name()}\n";
+        alertText += Footer();
+        return alertText;
+    }
+
+    private string Footer()
+    {
+        return "\nLove,\n\nThe Credit Card Company\n";
+    }
+
+    private string Greeting()
+    {
+        return $"Hello card user!\n\n";
+    }
+    
+    private bool IsEmpty(List<UnsusualSpending> unsusualSpendings)
+    {
+        return unsusualSpendings.Count == 0;
     }
 }
