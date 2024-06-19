f62683fcb3a5c74ed5c5cc5c4c154afe06e68cce
trikitrok <trikitrok@gmail.com>
2024-06-20 01:39:03 +0200

    extract inner class to compose alert text

--- budding-off/unusual-spendings-kata/UnusualSpendings/UnusualSpendingsService.cs
+++ budding-off/unusual-spendings-kata/UnusualSpendings/UnusualSpendingsService.cs
@@ -9,6 +9,8 @@ public class UnusualSpendingsService
     private readonly UserRepository _userRepository;
     private readonly AlertSender _alertsSender;
     private readonly UnusualSpendingsDetector _unusualSpendingsDetector;
+    private readonly AlertTextComposer _alertTextComposer;
+
 
     public UnusualSpendingsService(UnusualSpendingsDetector unusualSpendingsDetector, UserRepository userRepository,
         AlertSender alertsSender)
@@ -16,6 +18,7 @@ public class UnusualSpendingsService
         _unusualSpendingsDetector = unusualSpendingsDetector;
         _userRepository = userRepository;
         _alertsSender = alertsSender;
+        _alertTextComposer = new AlertTextComposer();
     }
 
     public void Alert(User user)
@@ -32,39 +35,47 @@ public class UnusualSpendingsService
 
     private Alert CreateAlert(List<UnsusualSpending> unsusualSpendings, UserContactData contactData)
     {
-        var alertText = ComposeAlertText(unsusualSpendings, contactData, out var userContactData);
+        var alertText = _alertTextComposer.ComposeAlertText(unsusualSpendings, contactData, out var userContactData);
         var alert = new Alert(alertText, userContactData);
         return alert;
     }
 
-    private string ComposeAlertText(List<UnsusualSpending> unsusualSpendings, UserContactData contactData,
-        out UserContactData userContactData)
-    {
-        var alertText = Greeting();
-        var unsusualSpending = unsusualSpendings.First();
-        userContactData = contactData;
-        var spendingCategories = unsusualSpending.SpendingCategories();
-        var spendingCategory = spendingCategories.First();
-        alertText += "We have detected unusually high spending on your card in these categories:\n\n" +
-                     $"* You spent {spendingCategory.CurrencySymbol()}" +
-                     $"{spendingCategory.TotalAmountspent().ToString("f2", new CultureInfo("en-US"))} " +
-                     $"on {spendingCategory.Name()}\n";
-        alertText += Footer();
-        return alertText;
-    }
-
-    private string Footer()
-    {
-        return "\nLove,\n\nThe Credit Card Company\n";
-    }
-
-    private string Greeting()
-    {
-        return $"Hello card user!\n\n";
-    }
-    
     private bool IsEmpty(List<UnsusualSpending> unsusualSpendings)
     {
         return unsusualSpendings.Count == 0;
     }
+
+    private class AlertTextComposer
+    {
+        public AlertTextComposer()
+        {
+        }
+
+        public string ComposeAlertText(List<UnsusualSpending> unsusualSpendings, UserContactData contactData,
+            out UserContactData userContactData)
+        {
+            var alertText = Greeting();
+            var unsusualSpending = unsusualSpendings.First();
+            userContactData = contactData;
+            var spendingCategories = unsusualSpending.SpendingCategories();
+            var spendingCategory = spendingCategories.First();
+            alertText += "We have detected unusually high spending on your card in these categories:\n\n" +
+                         $"* You spent {spendingCategory.CurrencySymbol()}" +
+                         $"{spendingCategory.TotalAmountspent().ToString("f2", new CultureInfo("en-US"))} " +
+                         $"on {spendingCategory.Name()}\n";
+            alertText += Footer();
+            return alertText;
+        }
+
+        private string Footer()
+        {
+            return "\nLove,\n\nThe Credit Card Company\n";
+        }
+
+        private string Greeting()
+        {
+            return $"Hello card user!\n\n";
+        }
+    }
+
 }
